a289bdee70a168463bdaf273e25ef5b256d4c58b
Gabriel Augusto <g.requenareis@gmail.com>
2021-06-01 01:10:21 -0300

    Factory creation

---
+++ database/factories/CuponFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\cupon;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CuponFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = cupon::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/factories/StatusPedidoFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\status_pedido;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class StatusPedidoFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = status_pedido::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
