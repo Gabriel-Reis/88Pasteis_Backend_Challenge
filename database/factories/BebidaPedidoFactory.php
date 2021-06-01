e4ab84251834659b2a12a02cbbf1091c6cfaa4d5
Gabriel Augusto <g.requenareis@gmail.com>
2021-06-01 04:52:23 -0300

    Factories and seeds updates

---
+++ database/factories/BebidaPedidoFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Bebida_pedido;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class BebidaPedidoFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Bebida_pedido::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
