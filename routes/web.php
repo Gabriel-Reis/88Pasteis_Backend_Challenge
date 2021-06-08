4c80a62ec4105aaf608c57f2c3551155259d3a95
Gabriel Augusto <g.requenareis@gmail.com>
2021-06-08 00:33:16 -0300

    Routes update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 use App\User;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CarrinhoController;
+use App\Http\Controllers\BebidaController;
+use App\Http\Controllers\BebidaPedidoController;
+use App\Http\Controllers\CupomController;
+use App\Http\Controllers\PagamentoController;
 use App\Http\Controllers\PastelController;
+use App\Http\Controllers\PastelPedidoController;
+use App\Http\Controllers\PedidoController;
+use App\Http\Controllers\StatusPedidoController;
+use App\Http\Controllers\SessionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,21 +28,26 @@ Auth::routes(['verify' => true]);
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'welcome'])->name('home');
 Route::get('/', [App\Http\Controllers\HomeController::class, 'welcome'])->name('/');
 
-
 // Email Sender Route
 Route::get('email_NewOrder',function(){
 	return new \App\Mail\NewOrder();  //Ver página HTML com email
 	// Illuminate\Support\Facades\Mail::send(new \App\Mail\NewOrder()); //Enviar email
 });
 
+Route::resource('carrinho', CarrinhoController::class);
+Route::resource('bebidas', BebidaController::class);
+Route::resource('bebida_pedido', BebidaPedidoController::class);
+Route::resource('cupons', CupomController::class);
+Route::resource('pagamentos', PagamentoController::class);
+Route::resource('pasteis', PastelController::class);
+Route::resource('pastel_pedido', PastelPedidoController::class);
+Route::resource('pedidos', PedidoController::class);
+Route::resource('status_pedido', StatusPedidoController::class);
 
- Route::resource('bebidas', PastelController::class);
- Route::resource('bebida_pedido', BebidaPedidoController::class);
- Route::resource('cupons', CupomController::class);
- Route::resource('pagamentos', PagamentoController::class);
- Route::resource('pasteis', PastelController::class);
- Route::resource('pastel_pedido', PastelPedidoController::class);
- Route::resource('pedidos', PedidoController::class);
- Route::resource('status_pedido', StatusPedidoController::class);
+
+
+// Route::get('session_form', function () { return view('ajax_session'); });
+Route::get('get_session', [SessionController::class, 'getsession']);
+Route::post('set_session', [SessionController::class, 'createsession'] );
 //Página com cardapio
 //Route::get('/cardapio', function () { return view('cardapio'); })->name('cardapio');;//->middleware('verified');
