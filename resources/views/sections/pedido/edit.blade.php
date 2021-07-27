8c8734b59d5d231b2fe8f3ffbb0e9b969ad77645
Gabriel Augusto <g.requenareis@gmail.com>
2021-07-26 21:01:20 -0300

    Remove unused code

--- resources/views/sections/pedido/edit.blade.php
+++ resources/views/sections/pedido/edit.blade.php
@@ -2,93 +2,6 @@
 
 @section('content')
 
-
-
-
-
-{{-- 	<div class="container">
-		<div class="row justify-content-center">
-			<div class="col-md-8">
-				<div class="card">
-					<div class="card-header text-center">Atualizar pedido</div>
-
-					<div class="card-body">
-
-						<div class="row mb-0">
-							<div class="col-sm-4 col-form-label">
-								Pastel
-							</div>
-							<div class="col-sm-2 col-form-label">
-								Quantidade
-							</div>
-							<div class="col-sm-3 col-form-label">
-								Preço unitário
-							</div>
-							<div class="col-sm-3 col-form-label">
-								Preço total
-							</div>
-						</div>
-
-						<form method="put" action="{{route('pedidos.update', $pedido->id)}}">
-							@foreach ($pastel_pedido as $pastel)
-							<div class="row mb-4">
-	                            <label for="{{$pasteis[$pastel->pastel_id-1]['titulo']}} " class="col-sm-4 col-form-label text-md-right">{{$pasteis[$pastel->pastel_id-1]['titulo']}} </label>
-
-	                            <div class='col-sm-2 col-form-label quantity buttons_added'><input type='button' value='-' class='minus order_edit_minus'><input type='number' id='{{$pastel->pastel_id-1}}qnt' step='1' min='0' max='' name='{{$pastel->pastel_id-1}}quantity' value='{{$pastel->quantidade}}' title='Qty' class='input-text qty text' size='4' pattern='' inputmode=''><input type='button' value='+' class='plus order_edit_plus'></div>
-
-	                            <div class="col-sm-3 col-form-label">
-	                            	<input class="form-control" type='text' id='{{$pastel->pastel_id-1}}unit' name='{{$pastel->pastel_id-1}}unit' value='R$ {{ number_format($pasteis[$pastel->pastel_id-1]['preco_unit'],2) }}' disabled>
-	                            </div>
-
-	                            <div class="col-sm-3 col-form-label">
-	                            	<input class="form-control" type='text' id='{{$pastel->pastel_id-1}}price' name='{{$pastel->pastel_id-1}}price' value='R$ {{ number_format($pasteis[$pastel->pastel_id-1]['preco_unit']*$pastel->quantidade,2) }}' disabled>
-	                            </div>
-	                        </div>
-							@endforeach
-
-							<hr>
-
-							<div class="form-group row container">
-							    <select id="add_new" class="form-select">
-									<option disabled selected value="0">Adicionar um novo pastel</option>
-									@foreach ($pasteis as $pastel)
-										@if(array_search($pastel->id, array_column($pastel_pedido->toArray(), 'pastel_id')) === false)
-											<option value="{{$pastel['id']}}">{{ $pastel['titulo'] }}</option>
-										@endif
-									@endforeach
-								</select>
-						    </div>							
-
-							<div class="form-group row container">
-								<label for="total" class="col-sm-4 col-form-label text-md-right">Total do pedido</label>
-	                            <div class="col-sm-5 col-form-label">
-	                            </div>						
-	                            <div class="col-sm-3 col-form-label">
-									<input class="form-control" type="text" value="R$ {{$pedido->total}}" disabled>
-						    	</div>							
-						    </div>							
-
-							<div class="form-group row container">
-								<input class="btn btn-success" type="submit" value="Atualizar pedido">
-						    </div>
-					  	</form>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div> --}}
-
-{{-- <table id="DataTable" class="table table-bordeless table-hover align">
-		<thead>
-			<tr>
-				<th>Pastel</th>
-				<th>Quantidade</th>
-				<th>Preço unitário</th>
-				<th>Preço total</th>
-			</tr>
-		</thead>
-	</table> --}}
-
 	<form>
 		<div class="row">
 			<div class="col-md-3">
@@ -158,6 +71,8 @@
 			</div>
 		</div>
 	</form>
+
+
 
 
 {{-- BUTTON ADD PASTEL AND REFRESH PAGE --}}
